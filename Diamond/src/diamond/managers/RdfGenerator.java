9fc626a43b62f28e6901f15190667e68fb851abd
Rick Liao <rickliao@utexas.edu>
2015-11-09 16:10:49 -0600

    finished stream of data

--- Diamond/src/diamond/managers/RdfGenerator.java
+++ Diamond/src/diamond/managers/RdfGenerator.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class RdfGenerator {
 	
@@ -28,19 +29,36 @@ public class RdfGenerator {
 			if((line = br.readLine()) != null) {
 				hasNext = true;
 				String[] data = line.split(",");
-				PrintWriter writer = new PrintWriter("rdf/the-file-name.txt", "UTF-8");
-				BufferedReader in = new BufferedReader(new FileReader("rdf/SampleEventService_Property-66462839-6a65-4d5a-acc6-ffb627d8dc14.xml"));
-	    	    String inLine;
-				while((inLine = in.readLine()) != null) {
-					if(inLine.contains("hasValue")) {
-						writer.println("        <sao:hasValue>"+data[0]+"</sao:hasValue>");
-					} else {
-						writer.println(inLine);
+				
+				String[] files = new String[]{
+						"rdf/SampleEventService_Property-7e96ab11-820a-42b9-aab2-b6c4ebf88de2.xml",
+						"rdf/SampleEventService_Property-66462839-6a65-4d5a-acc6-ffb627d8dc14.xml",
+						"rdf/SampleEventService_Property-5cb2e2d7-8773-4e66-9690-6cfb3591c17b.xml"
+				};
+				
+				// loop through the files need to be updated
+				for(int i = 0; i < files.length; i++) {
+					// read data
+					ArrayList<String> input = new ArrayList<String>();
+					BufferedReader in = new BufferedReader(new FileReader(files[i]));
+		    	    String inLine;
+					while((inLine = in.readLine()) != null) {
+						input.add(inLine);
 					}
+					in.close();
+					
+					// write data
+					PrintWriter writer = new PrintWriter(files[i], "UTF-8");
+					for(int j = 0; j < input.size(); j++) {
+						if(input.get(j).contains("hasValue")) {
+							writer.println("        <sao:hasValue>"+data[i]+"</sao:hasValue>");
+						} else {
+							writer.println(input.get(j));
+						}
+					}
+					writer.println();
+					writer.close();
 				}
-				in.close();
-				writer.println();
-				writer.close();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
